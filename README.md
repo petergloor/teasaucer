996494b9215af7f299bb1135c933e732309afab7
Peter Gloor <pgloor@gmail.com>
2024-03-13 17:11:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# teasaucer - a template for Bubble Tea (WIP)
+
+This is my very basic template for Go applications using the [Bubble Tea](https://github.com/charmbracelet/bubbletea) TUI framework.
+
+This is ongoing work in progress.
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	// "fmt"
+	"fmt"
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// model is a struct that holds all the stuff we need to store our
+// applications state.
+type model struct {
+	
+}
+
+// initialModel initializes and returns the model. This is not to 
+// be confused with the bubbletea.Init() method. This one prepares
+// our model, the other let's start with some kind of command.
+func initialModel() model {
+	return model{}
+}
+
+// Init is the first function that will be called. It returns an optional
+// initial command. To not perform an initial command return nil.
+func (m model) Init() tea.Cmd {
+	// For now, we do nothing here, so we hae nothing to return
+	return nil
+}
+
+// Update is called when a message is received. Use it to inspect messages
+// and, in response, update the model and/or send a command.
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+// View renders the program's UI, which is just a string. The view is
+// rendered after every Update.
+func (m model) View() string {
+	// s is a string that will be returned with our view for rendering.
+	s := ""
+
+	// Build the view
+	s += fmt.Sprintf("%s %s\n\n%s\n", ">", "Hello from 'tea saucer' ;)", "Press q to Quit.")
+
+	// Return the view for rendering.
+	return s
+}
+
+// The entry point of the appliction.
+func main() {
+	log.Println("Program 'tea saucer' started.")
+
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal("Error running tea saucer:", err)
+	}
+
+	log.Println("Program 'tea saucer' terminated.")
+}
